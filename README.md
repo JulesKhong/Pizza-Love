e9fde839e13194ce6051e9efb04a35e7af5d51f2
Julia Dickey <julia.dickey@gmail.com>
2016-06-17 16:53:27 -0700

    Final CSS styles, and edited README file

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ The Ma's Pizza site allows customers to order a pizza by selecting the size, and
 
 ## Setup/Installation Requirements
 
-* Clone the github repository. The site can be found [here](https://github.com/JuliaDickey/Pizza-Love) on GitHub, or click [here](ADD LINK TO LIVE SITE HERE) to view it on the web.
+* Clone the github repository. The site can be found [here](https://github.com/JuliaDickey/Pizza-Love) on GitHub, or click [here](http://juliadickey.github.io/Pizza-Love/ ) to view it on the web.
 * Open index.html in the browser of your choice.
 * Fill out the order form including pizza toppings and size
 * Click the _order_ button to see a summary of your order.
@@ -18,20 +18,18 @@ The Ma's Pizza site allows customers to order a pizza by selecting the size, and
 
 1. A user can select a pizza size, and the current total will reflect the size chosen.
   Example Input: "Medium"
-  Example Output: "15.00"
+  Example Output: "15"
 
-2. When a user adds toppings to their pizza, the price adjusts accordingly
-  Example Input: "pepperoni, pineapple, and jalapenos."
-  Example Output: pepperoni: 1.00, pineapple: .75, jalapenos: .50 Total: 17.25"
-
-3. When the user submits their order, an itemized summary appears.
+3. When the user submits their order, the total price for the the pizza will show up under the total.
   Example Input: Julia Dickey, 123 Lane Portland, OR
               1 medium pizza      15.00
                 with: pineapple   .75
                       pepperoni   1.00
                       jalapenos   .50
                       delivery:   5
-                      Total:      22.25
+  Example Output: Total:      22.25
+
+4. If a user doesn't submit a first name, a popup will tell them to please enter their name before they can submit their order.
 
 ## Known Bugs
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,11 @@
 // BACK END LOGIC
 function Pizza (delivery, pizzaSize, protein, veggie){
-  // this.orderName =
   this.delivery = delivery;
   this.pizzaSize = pizzaSize;
   this.protein = protein;
   this.veggie = veggie,
-  // this.extras = [],
   this.total = 10;
 }
-
 
 Pizza.prototype.addSizetoTotal = function(){
   if (this.pizzaSize === "large"){
@@ -49,11 +46,16 @@ $(document).ready(function(){
   $("#orderForm").submit(function(event){
     event.preventDefault();
 
+// GATHER DETAILS OF PIZZA ORDER
     var delivery = $("#transportMethod").val();
     var firstName = $("#firstName").val();
+
+    if (firstName === "" || firstName === null ){
+        $("#myModal").modal("show");
+      // alert("Oops! Looks like you forgot to enter your name");
+    } else {
     var lastName = $("#lastName").val();
     var orderedPizzaSize = $("#pizzaSize").val();
-    // var protein = $("input[name='protein']:checked").val();
     var protein = [];
     $.each($("input[name='protein']:checked"), function() {
       protein.push($(this).val());
@@ -64,160 +66,27 @@ $(document).ready(function(){
       veggie.push($(this).val());
     });
 
+// CREATE NEW OBJECT, AND RUN PROTOTYPES TO CALCULATE PIZZA COST
     var newPizza = new Pizza (delivery, orderedPizzaSize, protein, veggie);
     newPizza.addSizetoTotal();
     newPizza.addDeliverytoTotal();
     newPizza.addProtein();
     newPizza.addVeggie();
 
-    debugger;
-    $(".col-md-4").show;
+
+// ORDER SUMMARY INFO
+    $(".col-sm-4").show();
     $("#totalPrice").append(newPizza.total);
     $("#summarySize").append(newPizza.pizzaSize);
-
+    $("#customerName").append(firstName);
     $("#summaryToppings").append(protein.join(", "));
     $("#summaryToppings").append(", "+ veggie.join(", "));
 
-    console.log(newPizza.total);
+    if (delivery === "delivery"){
+      $("#summaryMessage").append("Confirm your order, and your pizza will be <strong><em>delivered</em></strong> to your door in 30 min or less.")
+    } else {
+      $("#summaryMessage").append("Confirm your order, and your pizza will be ready for <strong><em>pickup</em></strong> in 20 minutes.")
+    }
+  }
   })
-
 });
- // notes -------------------------------------------------------------
-//  function Pizza(size,sauce,toppings,cheese,quantity){
-//   this.size = size;
-//   this.sauce = sauce;
-//   this.toppings = toppings;
-//   this.cheese = cheese;
-//   this.quantity = quantity;
-// }
-//
-// var priceOptionsArray = [];
-// Pizza.prototype.basePrice = function(){
-//   if (this.size === "10"){
-//     return priceOptionsArray.push(8.99);
-//   } else if (this.size === "12"){
-//     priceOptionsArray.push(10.99);
-//   } else if (this.size === "14"){
-//     priceOptionsArray.push(14.99);
-//     return priceOptionsArray;
-//   } else{
-//     return "Please select a size!"
-//   }
-// }
-//
-// Pizza.prototype.addSauce = function(){
-//   var sauceList = [
-//     { sauce: "No sauce", price: 0.00},
-//     { sauce: "Marinara Sauce", price: 0.50},
-//     { sauce: "Barbecue Sauce", price: 1.00},
-//     { sauce: "Cashew Alfredo Sauce", price: 1.50}
-//   ];
-//   for (var prop in sauceList){
-//     if (sauceList[prop]['sauce'] === this.sauce){
-//      priceOptionsArray.push(sauceList[prop]['price']);
-//     }
-//   } return priceOptionsArray;
-// }
-//
-// Pizza.prototype.addToppings = function(){
-//   var toppingList = [
-//     { topping: "Field Roast Hamburger", price: 2.00},
-//     { topping: "Seitan Sausage", price: 2.00},
-//     { topping: "Chick'n", price: 2.00},
-//     { topping: "Barbecue Soy Curls", price: 2.00},
-//     { topping: "Jackfruit Bacon", price: 1.00},
-//     { topping: "Mushrooms", price: 0.50},
-//     { topping: "Spinach", price: 0.50},
-//     { topping: "Onions", price: 0.50},
-//     { topping: "Black Olives", price: 0.50},
-//     { topping: "Artichokes", price: 0.50},
-//     { topping: "Garlic", price: 0.50},
-//     { topping: "Pineapple", price: 0.50}
-//   ];
-//   for (var prop in toppingList){      //Searches through list of toppings and pushes price of matching into priceOptionsArray
-//     for (var i=0; i < this.toppings.length; i++){
-//       if (this.toppings[i] == toppingList[prop]['topping']){
-//        priceOptionsArray.push(toppingList[prop]['price']);
-//       }
-//     }
-//   } return priceOptionsArray;
-// }
-//
-// Pizza.prototype.addCheese = function(){
-//   var cheeseList = [
-//     { cheese: "No cheese", price: 0.00},
-//     { cheese: "Daiya Cheese", price: 0.50},
-//     { cheese: "Creamed Cashew Cheese", price: 1.50},
-//     { cheese: "Tofu Ricotta Cheese", price: 1.50}
-//   ];
-//   for (var prop in cheeseList){
-//     for (var i=0; i < this.cheese.length; i++){
-//       if (cheeseList[prop]['cheese'] == this.cheese[i]){
-//        priceOptionsArray.push(cheeseList[prop]['price']);
-//       }
-//     }
-//   } return priceOptionsArray;
-// }
-//
-// Pizza.prototype.totalPrice = function(){
-//   var finalPrice = 0;
-//   for (var i=0; i < priceOptionsArray.length; i++){
-//     finalPrice = finalPrice + priceOptionsArray[i];
-//   } return finalPrice.toFixed(2) * this.quantity;
-// }
-//
-// // --------User Interface Logic--------
-//
-// $(function() {
-//   $("button#submit").click(function(event) {
-//     event.preventDefault();
-//     $('#orderSummary, #orderPrice').empty();
-//     var pizzaSize = $("input[name='size']:checked").val();
-//     var pizzaSauce = $("input[name='sauce']:checked").val();
-//     var quantity = ($('input#quantity').val());
-//
-//     var pizzaToppings = [];
-//     $.each($("input[name='topping']:checked"), function() {
-//       pizzaToppings.push($(this).val());
-//     });
-//
-//     var pizzaCheese = [];
-//     $.each($("input[name='cheese']:checked"), function() {
-//     pizzaCheese.push($(this).val());
-//     });
-//
-//     var newPizza = new Pizza(pizzaSize,pizzaSauce,pizzaToppings,pizzaCheese,quantity);
-//     pizzaToppings = pizzaToppings.join(', ');
-//     pizzaCheese = pizzaCheese.join(', ');
-//
-//     if (quantity <= 0) {
-//       alert("Please enter a positive quantity");
-//     } else if ($("input[name='topping']:checked").length <= 0 ||  $("input[name='cheese']:checked").length <= 0){
-//       alert("Please select toppings/cheese");
-//     } else {
-//       $('#order').show();
-//
-//       $('#orderSummary').append(newPizza.quantity + " " + newPizza.size + '" pizza(s) with ' + newPizza.sauce.toLowerCase() + ", " + pizzaToppings.toLowerCase() + " and " + pizzaCheese.toLowerCase());
-//
-//       $('#orderPrice').append("Your total is: $" + newPizza.totalPrice(newPizza.basePrice(pizzaSize) + newPizza.addSauce(pizzaSauce) + newPizza.addToppings(pizzaToppings) +  newPizza.addCheese(pizzaCheese)));
-//       document.getElementById("pizzaForm").reset();
-//     }
-//   });
-// });
-
-
-// TRYING TO MAKE AN ARRAY WITH OBJECTS IN IT
-
-// Pizza.prototype.calculateTotal = function(){
-//   var baseprice = 10;
-//
-//   for (var i=0; i < pizzaSize.length ; i++ )
-//   if (this.pizzaSize === orderedPizzaSize){
-//     this.total += (pizzaSize.price);
-//   }
-  // else if (this.pizzaSize === "large"){
-  //   this.total += (baseprice + 10);
-  // } else {
-  //   this.total += baseprice;
-  // }
-// };
